9085b43c37980305eb2a2209c72144d545247919
kevin-zlz <1908456518@qq.com>
2018-10-17 10:33:46 +0800

    update traval by zlz 10-16

--- boke/views.py
+++ boke/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,JsonResponse,response
+import json
 # Create your views here.
+# 根据用户id查询用户编写的文章
+def queryWritebyuid(request):
+    pass
+
+# 根据用户id查询用户点赞过的文章
+def queryLikebyuid(request):
+    pass
+
+# 根据用户id查询用户评论过的文章
+def queryCommentbyuid(request):
+    pass
+
+# 根据文章id查询评论人
+def queryCommentbyaid(request):
+    pass
+
+# 根据文章id查询点赞人
+def queryLikebyaid(request):
+    pass
+
+# 添加文章
+def addAritical(request):
+    pass
+
+# 根据文章id添加评论
+def addComment(request):
+    pass
+
+# 根据文章id点赞
+def addLike(request):
+    pass
+
+# 根据文章id取消点赞
+def deletelike(request):
+    pass
